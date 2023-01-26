db595b87c0c0d39d15eb449a8dcb84f8e167383f
Kagan Degirmen <kagandegirmen@gmail.com>
2023-01-26 21:07:07 +0300

    First Commit > init

---
+++ digital-business-card/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NameTag from "./components/NameTag";
+import Info from "./components/Info";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
